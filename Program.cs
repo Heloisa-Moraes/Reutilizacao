e40de997c4f4072aa0b49e7eaf281a968da9a416
Heloisa-Moraes <helomoraes2009@hotmail.com>
2024-08-27 22:21:59 -0300

    correção realizada; validação de caractere especial;

--- Program.cs
+++ Program.cs
@@ -1,40 +1,60 @@
 using System;
 using System.Linq;
 
-public class Program 
+public class Program
 {
-    public static void Main (string[] args)
+    public static void Main(string[] args)
     {
         //Software de validação de senha 
 
-        String senhaCorreta = "123456";
+        Console.Write("Digite uma senha:");
+        String senha = ValidarSenha(Console.ReadLine());
 
-      Console.Write("Digite sua senha:");
-     string senha= Console.ReadLine();
+        Console.Write("\nConfirme sua senha:");
+        String confirmaSenha = ValidarSenha(Console.ReadLine());
 
-   Console.Write("Digite sua senha novamente: ");
-    string confirmasenha = Console.ReadLine();
-        //faz a validação
+        bool igual = ComparaTexto(senha, confirmaSenha);
 
-    bool tamanhoValido = senhaUsuario.Length >= 8;
-    bool contemNumero = senhaUsuario.Any(char.IsDigit); //essa função busca se o char é um digito 
-    bool contemMaiuscula = senhaUsuario.Any(char.IsUpper); // verifica se dentro da senha tem uma letra maiúscula
-
-    //testa se todos foram positivos
-    if(tamanhoValido && contemMaiuscula && contemNumero){
-        Console.WriteLine("Senha válida");
-    }else {
-        Console.WriteLine("Senha Inválida");
-
+        if (igual)
+            Console.WriteLine("\nArmazenando dados!");
+        else
+            Console.WriteLine("\nRefaca o processo");
     }
-        }
 
+    private static string ValidarSenha(string texto)
+    {
+        if (texto.Length < 8)
+            Console.WriteLine("Tamanho inválido");
+        if (!texto.Any(char.IsDigit))
+            Console.WriteLine("Não posssui numeros");
+        if (!texto.Any(char.IsUpper))
+            Console.WriteLine("Não possui letra maiuscula");
+        if (!texto.Any(c => !char.IsLetterOrDigit(c)))
+            Console.WriteLine("Nao possui caractere especial");
+
+
+
+        return texto;
+    }
+
+    private static bool ComparaTexto(string senha, string confirmaSenha)
+    {
+        if (senha == confirmaSenha)
+        {
+            Console.WriteLine("\nSenhas conferem");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("\n Senhas nao conferem");
+            return false;
+        }
+    }
 
 }
 
 
 
 
-        
-           
-         
+/* validar se tem um caractere especial e validar se a letra é maiuscula ou minuscula deixar passar ambas como corretas  */
+
